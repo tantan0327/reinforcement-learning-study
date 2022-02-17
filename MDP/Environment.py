c3a58aa45122f591633584749f8a851aa32555f2
Tange <tantantatange@gmail.com>
2022-02-18 01:40:21 +0900

    Created Environment class

--- MDP/Environment.py
+++ MDP/Environment.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class State():
+
     def __init__(self, row = -1, column = -1):
         self.row = row
         self.column = column
@@ -24,3 +25,52 @@ class Action(Enum):
     DOWN = -1
     LEFT = 2
     RIGHT = -2
+
+class Environment():
+
+    def __init__(self, grid, move_prob = 0.8):
+        # grid is 2-D array
+        # 0: ordinary cell
+        # -1: damage cell (game end)
+        # 1: reward cell (game end)
+        # 9: block cell
+        self.grid = grid
+        self.agent_state = State()
+
+        # reward is minus, so agent has to reach the goal fast
+        self.default_reward = -0.04
+
+        # agent can move to a selected direction in move_prob.
+        # move different direction in (1 - move_prob)
+        self.move_prob = move_prob
+        self.reset()
+
+    @property
+    def row_length(self):
+        return len(self.grid)
+
+    @property
+    def column_length(self):
+        return len(self.grid[0])
+
+    @property
+    def actions(self):
+        return [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT]
+
+    @property
+    def state(self):
+        states = []
+        for row in range(self.row_length):
+            for column in range(self.column_length):
+                # Block cells are not included
+                    if self.grid[row][column] != 9:
+                        states.append(State(row, column))
+
+        return states
+
+
+
+
+
+
+    
